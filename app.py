c99f290d1d5286a5db1c8881839d142eabf687b5
Atinkene <massinasylvanusbassene@gmail.com>
2025-05-09 15:04:57 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import streamlit as st
+
+# Charger le tokenizer et le modèle
+tokenizer = AutoTokenizer.from_pretrained("ac0hik/Sentiment_Analysis_French")
+model = AutoModelForSequenceClassification.from_pretrained("ac0hik/Sentiment_Analysis_French")
+
+# Créer un pipeline d’analyse de sentiments
+nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+# Exemple d'utilisation
+
+texte = st.text_area("Entrez un texte pour analyse de sentiment")
+if texte:
+    resultat = nlp(texte)
+    st.write(resultat)
+
+print(resultat)
